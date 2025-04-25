47b613182605b5b642695d42103eb258e7bb83fb
BhanuPrakash2047 <168528750+BhanuPrakash2047@users.noreply.github.com>
2025-04-25 23:25:07 +0530

    Add files via upload

---
+++ Backend/API-Gateway/src/test/java/com/example/gatewaygamma/GatewayGammaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaygamma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayGammaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
