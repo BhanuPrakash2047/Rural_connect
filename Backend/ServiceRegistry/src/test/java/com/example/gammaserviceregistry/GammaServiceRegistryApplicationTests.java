60873084c5c71a7ed7f954c9dc24d0d7efad3b32
BhanuPrakash2047 <168528750+BhanuPrakash2047@users.noreply.github.com>
2025-04-25 23:20:44 +0530

    Add files via upload

---
+++ Backend/ServiceRegistry/src/test/java/com/example/gammaserviceregistry/GammaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gammaserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GammaServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
